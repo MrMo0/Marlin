e21589c99f52d1aa39a61447f6098a6503303a1a
MrMo0 <50109142+MrMo0@users.noreply.github.com>
2019-05-04 23:33:01 +0200

    Change PID autotune settings 1/2
    Update BED PID settings

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -435,9 +435,9 @@
   //#define PID_BED_DEBUG // Sends debug data to the serial port.
 
   //Anycubic 4Max Ultrabase (0.9Ω @ 22°C)
-  #define DEFAULT_bedKp 251.78
-  #define DEFAULT_bedKi 49.57
-  #define DEFAULT_bedKd 319.73
+  #define DEFAULT_bedKp 198.89
+  #define DEFAULT_bedKi 37.93
+  #define DEFAULT_bedKd 260.69
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
